de582b6cbee28e61ff00be950cca972b9b0d576f
triks <trik0001000@gmail.com>
2024-05-15 18:25:32 +0300

    дописал логику возврата расчётов

--- solution/smo_over_queue.py
+++ solution/smo_over_queue.py
@@ -7,18 +7,24 @@ from dataclasses import dataclass
 class Iter:
     item: int
     random_count: float
-    
     time_between_applications: float
-    
     applications_time: float
-    
-    server: list
-    
+    server: list[float]
 
+@dataclass
+class Answer:
+    iters: list[Iter]
+    mathematical_expectations: float
 
+@dataclass
+class Queue:
+    answers: list[Answer]
+    average: float 
+
+    
 def time_between_applications(alpha, random_count):
     return round(-1 / alpha * math.log(random_count), 4)
-
+    
 def max_item_row(lst):
     return max(lst)
 
@@ -40,84 +46,76 @@ def max_item_columns(lst):
         
         
 def random_number_generator():
-    return round(random.uniform(0, 1), 4)
+    
+    if (number := round(random.uniform(0, 1), 4)) == 0:
+        return random_number_generator()
+    else:
+        return number
+
+def sum_answer(lst):
+    return sum([i.mathematical_expectations for i in lst])
 
 
-
-def simulate_queue(t1, t2, alpha, threads=1):
+def simulate_queue(t1, t2, alpha, threads=1, iterations=1):
     
-    random.seed(100)
+    answer_list: list[Answer] = []
     
-    iter_list: list[Iter] = []
-    
-    time = 0
-    random_count = 0
-    tba = 0
-    
-    servers = []  # Четыре канала обслуживания
-    servers_count_row = 0
-    
-    c = 0 
-    
-    while time <= t2:
-         
-        servers.append([])
+    for _ in range(iterations):
+        print(f"iteration {_}")
+        iter_list: list[Iter] = []
+        time = 0
+        random_count = 0
+        tba = 0
         
-        for _ in range(threads):
-            servers[-1].append(0)
+        servers = []
+        servers_count_row = 0
         
-        
-        column_max = max_item_columns(servers)
-        # print(column_max)
-        
-        for i, j in enumerate(column_max):
-            if time >= j:
-                servers[servers_count_row][i] = round(time + t1, 4)
-                break
-        else:
-            if i + 1 < len(servers[servers_count_row]):
-                servers[servers_count_row][i + 1] = round(time + t1, 4)
-            else:
-                column_max_min = min(column_max)
-                time = column_max_min
-                
-                servers[servers_count_row][column_max.index(column_max_min)] = round(time + t1, 4)
-                
-        servers_count_row += 1
-        
-        iter_list.append(Iter(
-            item=c, 
-            random_count=random_count, 
-            time_between_applications=tba, 
-            applications_time=time, 
-            server=servers[-1]
-        ))
-        
-    
-        random_count = random_number_generator()
-        tba = time_between_applications(alpha, random_count)
-        time = round(time + tba, 4)
-        
-        c += 1
-    
-    
-    
-    # table = PrettyTable()
-
-    # # Определяем основные столбцы
-    # main_columns = ['item', 'random_count', 'time_between_applications', 'applications_time']
-    # for col in main_columns:
-    #     table.add_column(col, [getattr(obj, col) for obj in iter_list])
-
-    # # Определяем максимальную длину списка server
-    # max_server_length = max(len(obj.server) for obj in iter_list)
-
-    # # Добавляем столбцы для элементов server
-    # for i in range(max_server_length):
-    #     table.add_column(f'server_{i+1}', [obj.server[i] if i < len(obj.server) else None for obj in iter_list])
-
-    # # Выводим таблицу
-    # print(table)
-    
+        c = 0 
+        while time <= t2:
             
-    # print(*iter_list, sep='\n')
+            servers.append([])
+            
+            for _ in range(threads):
+                servers[-1].append(0)
+            
+            
+            column_max = max_item_columns(servers)
+            
+            for i, j in enumerate(column_max):
+                if time >= j:
+                    servers[servers_count_row][i] = round(time + t1, 4)
+                    break
+            else:
+                if i + 1 < len(servers[servers_count_row]):
+                    servers[servers_count_row][i + 1] = round(time + t1, 4)
+                else:
+                    column_max_min = min(column_max)
+                    time = column_max_min
+                    
+                    servers[servers_count_row][column_max.index(column_max_min)] = round(time + t1, 4)
+                    
+            servers_count_row += 1
+            
+            iter_list.append(
+                Iter(
+                    item=c, 
+                    random_count=random_count, 
+                    time_between_applications=tba, 
+                    applications_time=time, 
+                    server=servers[-1]
+                )
+            )
+            
+        
+            random_count = random_number_generator()
+            tba = time_between_applications(alpha, random_count)
+            time = round(time + tba, 4)
+            
+            c += 1
+        
+        answer_list.append(Answer(iter_list[:], c - 1))
+    
+    
+    return Queue(answer_list, round(sum_answer(answer_list) / len(answer_list), 4))
+    
+queue = simulate_queue(1, 10, 5, 2, 100)
