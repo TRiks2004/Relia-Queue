20f5bf081a7dfb2f4f8940d1e351f33af8ef13ae
triks <trik0001000@gmail.com>
2024-05-16 16:54:29 +0300

    добавил автогинерацию страниц

--- router.py
+++ router.py
@@ -20,14 +20,14 @@ class ViewList(BaseSettings):
 
     # layout.html main_page.html qs_theory_page.html block_system_page.html cfr_refusal_page.html cfr_unlimited_page.html about.html                                                               ─╯
     layout: str = plus_html('layout')
-    
+    main_page: str = plus_html('main_page')
     
 view_list = ViewList()
 
 @main_point.get('/', response_class=HTMLResponse)
 def index(request: Request):
     return templates.TemplateResponse(
-        request=request, name=view_list.layout, context={'id': 1}
+        request=request, name=view_list.layout, context={'id': 1, "title": "Your Dynamic Title", 'dynamic_page': view_list.main_page}
     )
 
 
