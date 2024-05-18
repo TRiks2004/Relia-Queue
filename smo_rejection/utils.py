facd81a95415fc38b40f0d9c2822158ebfd02038
mbtmrw <keks2324098@gmail.com>
2024-05-18 11:10:35 +0300

    Удаление вывода

--- smo_rejection/utils.py
+++ smo_rejection/utils.py
@@ -50,28 +50,3 @@ def calculate_mean_served_requests(results):
     """
     total_served_requests = [result.served_requests for result in results]
     return round_value(sum(total_served_requests) / len(total_served_requests))
-
-def print_results(results, num_channels):
-    """
-    Выводит результаты симуляции в форматированном виде.
-
-    ### Параметры:
-    * `results (list[SimulationResult])` - Список объектов `SimulationResult`, содержащих результаты каждой итерации симуляции.
-    * `num_channels (int)` - Количество каналов обслуживания (потоков).
-    """
-    import pandas as pd
-
-    for result in results:
-        print(f"\n---------------------------- Cимуляция номер: {result.iteration} ----------------------------")
-        df = pd.DataFrame(result.request_times)
-
-        # Удаление дубликатов значений серверов
-        for i in range(1, len(df)):
-            for j in range(num_channels):
-                server_key = f'server_{j + 1}'
-                if df.at[i, server_key] == df.at[i - 1, server_key] or df.at[i, server_key] == '':
-                    df.at[i, server_key] = ''
-
-        print(df[['index', 'rand_value', 'iba', 'app_time', f'server_{1}', f'server_{2}', f'server_{3}', 'Обслужено', 'Отказов']].to_string(index=False))
-        print(f"\nКоличество исполненных заявок: {result.served_requests}")
-        print(f"Количество отказов: {result.rejected_requests}\n")
