69645f6255e99ae2ac234d941b4199db965e33cc
mbtmrw <keks2324098@gmail.com>
2024-05-18 11:04:58 +0300

    Добавление комментариев к коду

--- smo_rejection/processing.py
+++ smo_rejection/processing.py
@@ -1,15 +1,31 @@
-import numpy as np
 from models import SimulationParameters, SimulationResult
 from utils import generate_random_number, calculate_time
 
 def process_iteration(params: SimulationParameters, iteration: int) -> SimulationResult:
+    """
+    Выполняет одну итерацию симуляции обслуживания заявок.
+
+    ### Параметры:
+    * `params (SimulationParameters)` - Параметры симуляции.
+    * `iteration (int)` - Номер текущей итерации.
+
+    ### Возвращаемое значение:
+    * `SimulationResult` - Объект, содержащий результаты текущей итерации симуляции.
+
+    ### Описание:
+    Функция моделирует процесс поступления и обслуживания заявок в системе с несколькими каналами обслуживания (потоками).
+    На каждом шаге времени генерируется случайное значение, на основе которого рассчитывается интервал времени до следующей заявки.
+    Если в момент поступления заявки есть свободный канал обслуживания, заявка обслуживается, иначе она отклоняется.
+    Функция отслеживает время занятости каждого канала обслуживания и статистику обслуженных и отклоненных заявок.
+    Результаты итерации сохраняются в объекте `SimulationResult`, который возвращается функцией.
+    """
     T = params.T
     num_channels = params.num_channels
     service_time = params.service_time
     alfa = params.alfa
 
     request_times = []
-    service_end_times = np.zeros(num_channels)
+    service_end_times = [0] * num_channels
     served_requests = 0
     rejected_requests = 0
     request_time = 0
@@ -22,7 +38,7 @@ def process_iteration(params: SimulationParameters, iteration: int) -> Simulatio
         if request_time > T:
             break
 
-        available_server = np.argmin(service_end_times)
+        available_server = service_end_times.index(min(service_end_times))
         if service_end_times[available_server] <= request_time:
             service_end_times[available_server] = request_time + service_time
             served_requests += 1
--- smo_rejection/simulation.py
+++ smo_rejection/simulation.py
@@ -2,7 +2,37 @@ from exception import InvalidInputError, NumIterationsNegative, NumIterationsIsZ
 from models import SimulationParameters
 from processing import process_iteration
 
-def run_simulation(T: float, num_channels: int, service_time: float, num_iterations: int, alfa : int):
+def run_simulation(T: float, num_channels: int, service_time: float, num_iterations: int, alfa: int):
+    """
+    Запускает симуляцию обслуживания заявок с заданными параметрами.
+
+    ### Параметры:
+    * `T (float)` - Общее время симуляции.
+    * `num_channels (int)` - Количество каналов обслуживания (потоков).
+    * `service_time (float)` - Время обслуживания одной заявки.
+    * `num_iterations (int)` - Количество итераций симуляции.
+    * `alfa (int)` - Параметр для расчета интервалов между заявками.
+
+    ### Возвращаемое значение:
+    * `list[SimulationResult]` - Список объектов `SimulationResult`, содержащих результаты каждой итерации симуляции.
+
+    ### Исключения:
+    * `MaxTimeNegative` - Если `T` меньше или равно нулю.
+    * `NumChannelsNegative` - Если `num_channels` меньше или равно нулю.
+    * `NumChannelsIsZero` - Если `num_channels` равно нулю.
+    * `ServiceTimeNegative` - Если `service_time` меньше или равно нулю.
+    * `NumIterationsNegative` - Если `num_iterations` меньше нуля.
+    * `NumIterationsIsZero` - Если `num_iterations` равно нулю.
+    * `AlphaNegative` - Если `alfa` меньше нуля.
+    * `AlphaIsZero` - Если `alfa` равно нулю.
+
+    ### Описание:
+    Функция выполняет следующие действия:
+    1. Проверяет корректность входных параметров и выбрасывает соответствующее исключение в случае некорректных значений.
+    2. Создает объект `SimulationParameters` с заданными параметрами.
+    3. Запускает цикл по количеству итераций, на каждой итерации вызывая функцию `process_iteration` и сохраняя ее результат.
+    4. Возвращает список объектов `SimulationResult`, содержащих результаты каждой итерации симуляции.
+    """
     if T <= 0:
         raise MaxTimeNegative("Максимальное время симуляции должно быть положительным числом.")
     if num_channels <= 0:
@@ -19,8 +49,8 @@ def run_simulation(T: float, num_channels: int, service_time: float, num_iterati
         raise AlphaNegative("Альфа не может быть отрицательной")
     if alfa == 0:
         raise AlphaIsZero("Альфа не может быть равным нулю")
-    
-    params = SimulationParameters(T=T, num_channels=num_channels, service_time=service_time, num_iterations=num_iterations, alfa = alfa)
+
+    params = SimulationParameters(T=T, num_channels=num_channels, service_time=service_time, num_iterations=num_iterations, alfa=alfa)
     results = []
 
     for iteration in range(num_iterations):
