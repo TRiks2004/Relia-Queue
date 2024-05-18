d86766774252afa86af0443f79446f91bfd53e62
mbtmrw <keks2324098@gmail.com>
2024-05-18 13:01:26 +0300

    Изменения функции вызова

--- tests/printer.py
+++ tests/printer.py
@@ -19,14 +19,6 @@ def print_results(results, num_channels):
     for result in results:
         print(f"\n---------------------------- Cимуляция номер: {result.iteration} ----------------------------")
         df = pd.DataFrame(result.request_times)
-
-        # Удаление дубликатов значений серверов
-        for i in range(1, len(df)):
-            for j in range(num_channels):
-                server_key = f'server_{j + 1}'
-                if df.at[i, server_key] == df.at[i - 1, server_key] or df.at[i, server_key] == '':
-                    df.at[i, server_key] = ''
-
         print(df[['index', 'rand_value', 'iba', 'app_time', f'server_{1}', f'server_{2}', f'server_{3}', 'Обслужено', 'Отказов']].to_string(index=False))
         print(f"\nКоличество исполненных заявок: {result.served_requests}")
         print(f"Количество отказов: {result.rejected_requests}\n")
