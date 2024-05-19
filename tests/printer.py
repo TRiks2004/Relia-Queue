a18b89d9f758ccfe506e055f32630d8cd1dff47d
uurixan <wp31012000@gmail.com>
2024-05-19 14:59:32 +0300

    Добавление метода с экспортом в пдф

--- tests/printer.py
+++ tests/printer.py
@@ -30,20 +30,8 @@ def main():
     num_iterations = 2
     alfa = 5
 
-    try:
-        # Запуск симуляции с заданными параметрами
-        results = run_simulation(T, num_channels, service_time, num_iterations, alfa)
-        
-        # Печать результатов симуляции
-        print_results(results, num_channels)
-        
-        # Расчет и печать среднего числа обслуженных заявок
-        a = calculate_mean_served_requests(results)
-        print("\nВ качестве оценки искомого математического ожидания a – числа обслуженных заявок примем выборочную среднюю:")
-        print(f"a = {a}")
-    except Exception as e:
-        # Обработка исключений
-        print(f"Произошла ошибка: {e}")
+    results = run_simulation(T, num_channels, service_time, num_iterations, alfa)
+    export_to_pdf(results, "simulation_results.pdf")
 
 if __name__ == "__main__":
     main()
