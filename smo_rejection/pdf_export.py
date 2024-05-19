f6cbc0e1243ca101696574c7bc30ff34ec6f800b
uurixan <wp31012000@gmail.com>
2024-05-19 15:15:27 +0300

    Создание логики для экспорта в пдф

--- smo_rejection/pdf_export.py
+++ smo_rejection/pdf_export.py
@@ -1,20 +1,27 @@
 from reportlab.lib.pagesizes import letter
-from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Spacer
-from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle, Paragraph, Spacer
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
 from reportlab.lib import colors
+from reportlab.pdfbase.ttfonts import TTFont
+from reportlab.pdfbase import pdfmetrics
 from .utils import calculate_mean_served_requests
 
 def export_to_pdf(results, filename):
+    pdfmetrics.registerFont(TTFont('Arial', 'arial.ttf'))
+
     styles = getSampleStyleSheet()
-    doc = SimpleDocTemplate(filename, pagesize=letter)
+    styles.add(ParagraphStyle(name='Cyrillic', fontName='Arial', fontSize=10))
+    styles.add(ParagraphStyle(name='CyrillicHeading3', fontName='Arial', fontSize=12, spaceAfter=6))
+
+    doc = SimpleDocTemplate(filename, pagesize=letter, encoding='UTF-8')
     elements = []
 
     for i, result in enumerate(results, start=1):
-        header = f"---------------------------- Cимуляция номер: {i} ----------------------------"
+        header = Paragraph(f"----------------------------------------- Симуляция номер: {i} -----------------------------------------", styles["CyrillicHeading3"])
         elements.append(header)
 
-        table_data = [["index", "rand_value", "iba", "app_time", "server_1", "server_2", "server_3", "Обслужено", "Отказов"]]
+        table_data = [["Индекс", "Случайное число", "МЕЖ", "Время в очереди", "Сервер 1", "Сервер 2", "Сервер 3", "Обслужено", "Отказов"]]
         for entry in result.request_times:
             row = [entry["index"], entry["rand_value"], entry["iba"], entry["app_time"]]
             for j in range(1, 4):
@@ -24,7 +31,7 @@ def export_to_pdf(results, filename):
 
         table = Table(table_data)
         table.setStyle(TableStyle([
-            ('FONTNAME', (0, 0), (-1, -1), 'Courier'),
+            ('FONTNAME', (0, 0), (-1, -1), 'Arial'),
             ('FONTSIZE', (0, 0), (-1, -1), 8),
             ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
             ('INNERGRID', (0, 0), (-1, -1), 0.25, colors.black),
@@ -32,13 +39,13 @@ def export_to_pdf(results, filename):
         ]))
         elements.append(table)
 
-        served_count = f"Количество исполненных заявок: {result.served_requests}"
-        rejected_count = f"Количество отказов: {result.rejected_requests}"
+        served_count = Paragraph(f"Количество исполненных заявок: {result.served_requests}", styles["Cyrillic"])
+        rejected_count = Paragraph(f"Количество отказов: {result.rejected_requests}", styles["Cyrillic"])
         elements.append(served_count)
         elements.append(rejected_count)
         elements.append(Spacer(1, 0.5 * inch))
 
-    mean_served = f"В качестве оценки искомого математического ожидания a – числа обслуженных заявок примем выборочную среднюю: a = {calculate_mean_served_requests(results)}"
+    mean_served = Paragraph(f"В качестве оценки искомого математического ожидания a – числа обслуженных заявок примем выборочную среднюю: a = {calculate_mean_served_requests(results)}", styles["Cyrillic"])
     elements.append(mean_served)
 
     doc.build(elements)
--- tests/printer.py
+++ tests/printer.py
@@ -3,25 +3,7 @@ import os
 
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-from smo_rejection import run_simulation, calculate_mean_served_requests
-
-
-def print_results(results, num_channels):
-    """
-    Выводит результаты симуляции в форматированном виде.
-
-    ### Параметры:
-    * `results (list[SimulationResult])` - Список объектов `SimulationResult`, содержащих результаты каждой итерации симуляции.
-    * `num_channels (int)` - Количество каналов обслуживания (потоков).
-    """
-    import pandas as pd
-
-    for result in results:
-        print(f"\n------------------------------- Cимуляция номер: {result.iteration} -------------------------------")
-        df = pd.DataFrame(result.request_times)
-        print(df[['index', 'rand_value', 'iba', 'app_time', f'server_{1}', f'server_{2}', f'server_{3}', 'Обслужено', 'Отказов']].to_string(index=False))
-        print(f"\nКоличество исполненных заявок: {result.served_requests}")
-        print(f"Количество отказов: {result.rejected_requests}\n")
+from smo_rejection import run_simulation, calculate_mean_served_requests, export_to_pdf
 
 def main():
     T = 4
