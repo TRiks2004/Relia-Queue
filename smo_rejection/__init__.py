6658c7995c66a6640308a3ee2b230da21fc98d11
uurixan <wp31012000@gmail.com>
2024-05-20 10:58:49 +0300

    Обновление версии библиотеки

--- smo_rejection/__init__.py
+++ smo_rejection/__init__.py
@@ -1,5 +1,6 @@
 from .simulation import run_simulation
-from .utils import calculate_mean_served_requests
+from .processing import process_iteration
+from .utils import calculate_mean_served_requests, round_value, calculate_time
 from .models import SimulationParameters, SimulationResult
 from .pdf_export import export_to_pdf
 from .xml_export import export_to_excel
